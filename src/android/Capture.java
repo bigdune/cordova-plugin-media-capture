b4a5a6c7aa9ed336fdfb9b63850663dab7e13a29
Limichilwe Simwanza <limichilwe@gmail.com>
2026-01-12 20:51:29 +0200

    Passed null to check permissions

--- src/android/Capture.java
+++ src/android/Capture.java
@@ -318,7 +318,7 @@ public class Capture extends CordovaPlugin {
      * Sets up an intent to capture images.  Result handled by onActivityResult()
      */
     private void captureImage(Request req) {
-        if (isMissingCameraPermissions(req, Manifest.permission.READ_EXTERNAL_STORAGE)) return;
+        if (isMissingCameraPermissions(req, null)) return;
 
         // Save the number of images currently on disk for later
         this.numPics = queryImgDB(whichContentStore()).getCount();
@@ -344,7 +344,7 @@ public class Capture extends CordovaPlugin {
      * Sets up an intent to capture video.  Result handled by onActivityResult()
      */
     private void captureVideo(Request req) {
-        if (isMissingCameraPermissions(req, Manifest.permission.READ_EXTERNAL_STORAGE)) return;
+        if (isMissingCameraPermissions(req, null)) return;
 
         Intent intent = new Intent(android.provider.MediaStore.ACTION_VIDEO_CAPTURE);
         
